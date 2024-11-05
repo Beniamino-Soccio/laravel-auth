8d50b22fe703849f7e836655179b345163a6e750
Beniamino-Soccio <beniamino43@gmail.com>
2024-11-05 21:45:42 +0100

    populated index

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,6 +2,45 @@
 
 @section("content")
 
-    <h1>Admin-index</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="py-3 fw-bold text-center">
+                    Projects list:
+                </h1>
+            </div>
+            <div class="col-12">
+                {{-- <div class="mb-3">
+                    <a href="{{ route("project.create") }}" class="btn btn-primary btn-lg">
+                        Create new character
+                    </a>
+                </div> --}}
 
+                @forelse ( $projects as $index => $project )
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $project->title }}</h5>
+                            <p class="card-text">{{ $project->info }}</p>
+                            <a href="{{ $project->url }}" class="card-link">Repository GitHUb</a>
+                            <a href="{{ route("project.show", $project->id) }}" class="btn btn-sm btn-primary me-2">Show</a>
+                            <a href="{{ route("project.edit", $project->id) }}" class="btn btn-sm btn-success me-2">Edit</a>
+                            <form action="{{ route("project.delete", $project->id) }}" method="POST" class="d-inline project-destroyer" custom-data-name="{{ $project->name }}">
+                                @csrf
+                                @method("DELETE")
+
+                                <button type="submit" class="btn btn-sm btn-warning"> Delete </button>
+                            </form>
+                        </div>
+                    </div>
+                @empty
+                    <p>No role available</p>
+                @endforelse
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+@section("additional-scripts")
+    @vite("resources/js/pokemons/delete-confirmation.js")
 @endsection
