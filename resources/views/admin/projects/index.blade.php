e184bdfa1adb7f55a251314ee7de66c4b965e614
Beniamino-Soccio <beniamino43@gmail.com>
2024-11-05 21:51:24 +0100

    fixed index

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -3,13 +3,13 @@
 @section("content")
 
     <div class="container">
-        <div class="row">
+        <div class="row justify-content-center">
             <div class="col-12">
                 <h1 class="py-3 fw-bold text-center">
                     Projects list:
                 </h1>
             </div>
-            <div class="col-12">
+            <div class="col-8">
                 {{-- <div class="mb-3">
                     <a href="{{ route("project.create") }}" class="btn btn-primary btn-lg">
                         Create new character
@@ -17,19 +17,19 @@
                 </div> --}}
 
                 @forelse ( $projects as $index => $project )
-                    <div class="card">
+                    <div class="card mb-4">
                         <div class="card-body">
-                            <h5 class="card-title">{{ $project->title }}</h5>
+                            <h5 class="card-title fw-bold fs-3">{{ $project->title }}</h5>
                             <p class="card-text">{{ $project->info }}</p>
                             <a href="{{ $project->url }}" class="card-link">Repository GitHUb</a>
-                            <a href="{{ route("project.show", $project->id) }}" class="btn btn-sm btn-primary me-2">Show</a>
+                            {{-- <a href="{{ route("project.show", $project->id) }}" class="btn btn-sm btn-primary me-2">Show</a>
                             <a href="{{ route("project.edit", $project->id) }}" class="btn btn-sm btn-success me-2">Edit</a>
                             <form action="{{ route("project.delete", $project->id) }}" method="POST" class="d-inline project-destroyer" custom-data-name="{{ $project->name }}">
                                 @csrf
                                 @method("DELETE")
 
                                 <button type="submit" class="btn btn-sm btn-warning"> Delete </button>
-                            </form>
+                            </form> --}}
                         </div>
                     </div>
                 @empty
